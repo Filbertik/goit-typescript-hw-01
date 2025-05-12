3cd0b631b47b281a2ef493653a0f178d80ddcd98
filbertik <aton2019zvit@gmail.com>
2025-05-12 14:24:11 +0300

    fix

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,8 @@
-function merge<T, U>(objA: T, objB: U): T & U {
+// function merge<T, U>(objA: T, objB: U): T & U {
+//   return Object.assign({}, objA, objB);
+// }
+
+// Fix edits tell by mentor
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
